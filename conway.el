32984ecee476c76e7fc07b7b3b8cbeac4eba2084
Felix Geller <fgeller@gmail.com>
2013-05-11 21:43:56 +1200

    Should cell's live?

--- conway.el
+++ conway.el
@@ -5,10 +5,12 @@
 (defun cell-y (c) (cdr c))
 (defun cell-neighbors (cell)
   (let (neighbors)
-    (dolist (new-x '(-1 0 1))
-      (dolist (new-y '(-1 0 1))
-        (when (not (equal (cell new-x new-y) cell))
-          (add-to-list 'neighbors (cell new-x new-y)))))
+    (dolist (x-offset '(-1 0 1))
+      (dolist (y-offset '(-1 0 1))
+        (let ((new-cell (cell (- (cell-x cell) x-offset)
+                              (- (cell-y cell) y-offset))))
+          (when (not (equal new-cell cell))
+            (add-to-list 'neighbors new-cell)))))
     neighbors))
 
 
@@ -18,9 +20,33 @@
     (let ((next-cell (car board)))
       (or (and (equal x (cell-x next-cell)) (equal y (cell-y next-cell)))
           (board-living-cell-p (cdr board) x y)))))
+(defun board-cell-will-live-p (board cell)
+  (let ((living-neighbor-count 0))
+    (dolist (neighbor (cell-neighbors cell))
+      (when (board-living-cell-p board (cell-x neighbor) (cell-y neighbor))
+        (setq living-neighbor-count (1+ living-neighbor-count))))
+    (or
+     (and (equal 2 living-neighbor-count)
+          (board-living-cell-p board (cell-x cell) (cell-y cell)))
+     (equal 3 living-neighbor-count))))
 
 
 (ert-run-tests-interactively "^conway-test" " *ert conway-tests*")
+
+
+(ert-deftest conway-test:board-cell-will-live ()
+  (let* ((test-board (board (list
+                             (cell 0 0) (cell 1 0) (cell 2 0)
+                             (cell 0 1)            (cell 2 1) (cell 3 1)
+                                                   (cell 2 2)))))
+    (cl-assert (not (board-cell-will-live-p test-board (cell 2 -1))))
+    (cl-assert (board-cell-will-live-p test-board (cell 0 0)))
+    (cl-assert (not (board-cell-will-live-p test-board (cell 1 0))))
+    (cl-assert (board-cell-will-live-p test-board (cell 3 0)))
+    (cl-assert (board-cell-will-live-p test-board (cell 0 1)))
+    (cl-assert (not (board-cell-will-live-p test-board (cell 1 1))))
+    (cl-assert (not (board-cell-will-live-p test-board (cell 2 1))))
+    (cl-assert (board-cell-will-live-p test-board (cell 3 2)))))
 
 
 (ert-deftest conway-test:cell-neighbors ()
